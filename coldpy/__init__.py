942770328a25ae460961febb3cd73cb45e439f0f
Geoff Ower <gdower@illinois.edu>
2020-09-09 10:56:53 -0500

    wip

--- coldpy/__init__.py
+++ coldpy/__init__.py
@@ -2,10 +2,13 @@ from .description import Description
 from .distribution import Distribution
 from .media import Media
 from .name import Name
+from .name import Names
 from .name_rel import NameRel
 from .reference import Reference
 from .synonym import Synonym
+from .synonym import Synonyms
 from .taxon import Taxon
+from .taxon import Taxa
 from .type_material import TypeMaterial
 from .vernacular_name import VernacularName
 
